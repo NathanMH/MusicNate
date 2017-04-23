030e9cbd03d3fe801b13bb2399527c2f5b36cf6d
NathanMH <nathan@musicnate.ca>
2017-04-23 12:01:48 -0700

    Add windows file path for content.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,8 @@ AUTHOR = u'Nathan Mador-House'
 SITENAME = u'MusicNate'
 SITEURL = ''
 
-PATH = '/home/musicnate/Documents/MusicNate/content/'
+#PATH = '/home/musicnate/Documents/MusicNate/content/'
+PATH = 'C:\\Users\\natha\\Documents\\MusicNate\\content\\'
 
 TIMEZONE = 'America/Toronto'
 
