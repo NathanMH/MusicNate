7bca2ff1fc734ea4c418725771d4f6a81f58c934
NathanMH <nathan@musicnate.ca>
2018-05-09 14:08:33 -0400

    update pelican.conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Nathan Mador-House'
 SITENAME = u'MusicNate'
-SITEURL = 'https://nathanmh.github.io'
+SITEURL = 'https://www.musicnate.ca'
 
 PATH = '/home/musicnate/Documents/MusicNate/content/'
 #PATH = 'C:\\Users\\natha\\Documents\\NathanMH.github.io\\content\\'
